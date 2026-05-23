1a91db56ef67d12fb8e7baf5995ca269047be27c
admin <dtr3734@gmail.com>
2026-05-23 12:17:03 +0300

    feat: Django admin for Exercise with inline alias editor

--- workouts/admin.py
+++ workouts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Exercise, ExerciseAlias
 
-# Register your models here.
+
+class ExerciseAliasInline(admin.TabularInline):
+    model = ExerciseAlias
+    extra = 1
+    fields = ('alias',)
+
+
+@admin.register(Exercise)
+class ExerciseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'equipment', 'movement_type', 'is_active')
+    list_filter = ('category', 'equipment', 'movement_type', 'is_active')
+    search_fields = ('name', 'muscle_group')
+    inlines = [ExerciseAliasInline]
+    ordering = ('category', 'name')
